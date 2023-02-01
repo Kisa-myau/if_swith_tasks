86ca7a99b12626038c346ac4b26d8c9f5f522993
Anna Perechneva <perechneva_anna@mail.ru>
2023-02-01 14:38:57 +0400

    If_Swithch_tasks

---
+++ src/task3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class task3 {
+    static String isYearVisososniy (int a){
+        if (a % 4 == 0 & a%100!=0||a%400==0) return "YES";
+        else return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите год - целое, положительное, не превышающее 30000 число ");
+        int a = in.nextInt();
+        in.close();
+
+        if (a<30000&a>0) System.out.println(isYearVisososniy(a));
+        else System.out.println("Введите число из заданного диапазона!");
+    }
+}
+
